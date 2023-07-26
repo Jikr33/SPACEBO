06de4331ee7e5769e794d10f7c4c83d5c04e05b6
Jikr33 <munkhuusuld@gmail.com>
2023-07-26 20:18:23 +0800

    highscore fixed?

--- src/supas/supabaseAddUser.ts
+++ src/supas/supabaseAddUser.ts
@@ -1,45 +1,49 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || ''
-const supabaseKey = process.env.REACT_APP_SUPABASE_API_KEY || ''
-const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || "";
+const supabaseKey = process.env.REACT_APP_SUPABASE_API_KEY || "";
+const supabase = createClient(supabaseUrl, supabaseKey);
 
+interface supaType {
+  highest_score: number;
+  h: string;
+}
 
-export async function supabaseAddUser(name:string)  {
-
+export async function supabaseAddUser(name: string) {
   let { data: userExist, error } = await supabase
-  .from('Spacebo Scores')
-  .select('*')
-  .eq('player_name', name)
+    .from("Spacebo Scores")
+    .select("*")
+    .eq("player_name", name);
 
   if (!error) {
-    console.log(userExist)
+    console.log(userExist);
     if (userExist?.length === 0) {
-    // console.log(userExist, 'user does not exist, will try to add new user')
-    // When user does not exist, user will be added to database, with 0 score.
-      const { data, error } = await supabase
-      .from('Spacebo Scores')
-      .insert([
-        { player_name: name, highest_score: 0 },
-      ])
-      .select()
+      // console.log(userExist, 'user does not exist, will try to add new user')
+      // When user does not exist, user will be added to database, with 0 score.
+      const { data: o, error } = await supabase
+        .from("Spacebo Scores")
+        .insert([{ player_name: name, highest_score: 0 }])
+        .select();
       if (!error) {
-        console.log(data, `${name} was added to database`)
+        console.log(o, `${name} was added to database`);
+        localStorage.setItem("highscore", "0");
       }
     }
     // when user already exists, update last_visited_at.
     else {
       const { data, error } = await supabase
-      .from('Spacebo Scores')
-      .update({ last_visited_at:'now()' })
-      .eq('player_name', name)
-      .select()
+        .from("Spacebo Scores")
+        .update({ last_visited_at: "now()" })
+        .eq("player_name", name)
+        .select();
 
-      if(!error) {
-        console.log(data, 'updataed')
+      if (!error) {
+        data.map((player: any) => {
+          localStorage.setItem("highscore", player.highest_score);
+          return true;
+        });
+        console.log(data, "updataed");
       }
     }
   }
-
-
 }
